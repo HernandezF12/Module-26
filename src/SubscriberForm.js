3c8102f60d5f247b5c1ae3443871558e50e16033
Felipe Laptop Hernandez <hernandezfjr15@gmail.com>
2022-11-29 18:29:17 -0600

    these are most of the files created during the module 26 lessons.

---
+++ src/SubscriberForm.js
@@ -0,0 +1,115 @@
+import React, {useState} from "react";
+
+function SubscriberForm() {
+    const initialFormState = {
+      name: "Chris",
+      email: "deez@ynuts.com",
+      referral: "twitter",
+      age:"high",
+      subscription:""
+    };
+    const [formData, setFormData] = useState({ ...initialFormState });
+    const handleChange = ({ target }) => {
+      setFormData({
+        ...formData,
+        [target.name]: target.value,
+      });
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log("Submitted:", formData);
+      setFormData({ ...initialFormState });
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">
+          Enter Your Name:
+          <input
+            id="name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            value={formData.nam}
+            />
+            </label>
+            <br />
+            <label htmlFor="email">
+                Your Email: 
+                <input 
+                id="email"
+                type="email"
+                name="email"
+                onChange={handleChange}
+                value={formData.email}
+                />
+                </label>
+                <br />
+                <button type="submit">Submit</button>
+                <label htmlFor="referral">
+                    How did you hear about us?
+                    <select
+                    id="referral"
+                    name="referral"
+                    onChange={handleChange}
+                    value={formData.referral}
+                    >
+                        <option value="">-- Select an Option -- </option>
+                        <option value="twitter">Twitter</option>
+                        <option value="wom">Word of Mouth</option>
+                        <option value="youtube">youtube</option>
+                    </select>
+                    </label>
+                    <fieldset>
+                        <legend>How old are you?</legend>
+                        <label htmlFor="low">
+                            Under 18
+                            <input 
+                            id="low"
+                            type="radio"
+                            name="age"
+                            onchange={handleChange}
+                            value="low"
+                            checked={formData.age === "low"}
+                            />
+                        </label>
+                        <label htmlFor="middle">
+                            18 - 60 
+                            <input 
+                            id="middle"
+                            type="radio"
+                            name="age"
+                            onChange={handleChange}
+                            value="middle"
+                            checked={formData.age === "middle"}
+                            />
+                            </label>
+                            <label htmlFor="high">
+                                Over 60
+                                <input 
+                                id="high"
+                                type="radio"
+                                name="age"
+                                onChange={handleChange}
+                                value="high"
+                                checked={formData.age === "high"}
+                                />
+                            </label>
+                    </fieldset>
+                    <label htmlFor="subscription">
+                        Receive email notifications?
+                        <input 
+                        id="subscription"
+                        type="checkbox"
+                        name="subscription"
+                        onChange={handleChange}
+                        checked={formData.subscription}
+                        value="subscription"
+                        />
+                    </label>
+                </form>
+    );
+}
+
+export default SubscriberForm;
